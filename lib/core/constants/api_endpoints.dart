d9133f8d83035f0f772c16c79a876244c59c8ffb
CoupDerace <turtusibae@gmail.com>
2026-06-12 15:20:09 +0700

    feat: add OTP endpoint constants
    Menambahkan:
    sendOtpFirebase
    sendOtpEmail
    confirmOtp
    totpRegister
    totpVerify

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -12,4 +12,13 @@ class ApiEndpoints {
   static const String verifyEmailOtp = '$_base/auth/verify-email-otp';
   static const String me = '$_base/auth/me';
   static const String fcmToken = '$_base/auth/fcm-token';
+
+  //OTP
+  static const String sendOtpFirebase = '$_base/otp/send-firebase';
+  static const String sendOtpEmail = '$_base/otp/send-email';
+  static const String confirmOtp = '$_base/otp/confirm';
+  static const String totpRegister = '$_base/otp/totp/register';
+  static const String totpVerify = '$_base/otp/totp/verify';
+
+  
 }
