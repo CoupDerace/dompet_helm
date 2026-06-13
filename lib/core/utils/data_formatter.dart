bc002976df8eebb72e1cbf635d52fd7cfbd279cf
CoupDerace <turtusibae@gmail.com>
2026-06-13 10:07:01 +0700

    feat(utils): add Indonesian short date formatter
    cakupan: format tanggal singkat menggunakan locale id_ID

--- lib/core/utils/data_formatter.dart
+++ lib/core/utils/data_formatter.dart
@@ -5,6 +5,13 @@ class DateFormatter {
     final yesterday = today.subtract(const Duration(days: 1));
     final date = DateTime(dt.year, dt.month, dt.day);
   
-  
+    final timeStr = DateFormat('HH.mm').format(dt);
+    if (date == today) return 'Hari ini, $timeStr';
+    if (date == yesterday) return 'Kemarin, $timeStr';
+    return '${DateFormat('d MMM', 'id_ID').format(dt)}, $timeStr';
+  }
+
+  static String formatShort(DateTime dt) {
+    return DateFormat('d MMM yyyy', 'id_ID').format(dt);
   }
 }
