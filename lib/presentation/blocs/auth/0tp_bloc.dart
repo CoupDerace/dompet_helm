bca4297767f0183f9ade1164b340d30963085c0c
CoupDerace <turtusibae@gmail.com>
2026-06-13 23:25:03 +0700

    feat(otp): implement OTP bloc
    cakupan: implementasi OtpBloc menggunakan flutter_bloc

--- lib/presentation/blocs/auth/0tp_bloc.dart
+++ lib/presentation/blocs/auth/0tp_bloc.dart
@@ -1,5 +1,3 @@
-
-
 // Events
 abstract class OtpEvent extends Equatable {
   @override
@@ -7,7 +5,9 @@ abstract class OtpEvent extends Equatable {
 }
 
 class OtpSendFirebase extends OtpEvent {}
+
 class OtpSendEmail extends OtpEvent {}
+
 class OtpConfirm extends OtpEvent {
   final String code;
   final String otpType;
@@ -15,13 +15,16 @@ class OtpConfirm extends OtpEvent {
   @override
   List<Object?> get props => [code, otpType];
 }
+
 class OtpRegisterTotp extends OtpEvent {}
+
 class OtpVerifyTotp extends OtpEvent {
   final String code;
   OtpVerifyTotp(this.code);
   @override
   List<Object?> get props => [code];
 }
+
 class OtpReset extends OtpEvent {}
 
 // States
@@ -31,30 +34,41 @@ abstract class OtpState extends Equatable {
 }
 
 class OtpInitial extends OtpState {}
+
 class OtpLoading extends OtpState {}
+
 class OtpSent extends OtpState {
   final OtpSentEntity entity;
   OtpSent(this.entity);
   @override
   List<Object?> get props => [entity];
 }
+
 class OtpVerified extends OtpState {}
+
 class OtpTotpSetup extends OtpState {
   final TotpSetupEntity entity;
   OtpTotpSetup(this.entity);
   @override
   List<Object?> get props => [entity];
 }
+
 class OtpTotpEnabled extends OtpState {}
+
 class OtpInvalid extends OtpState {
   final String message;
   OtpInvalid(this.message);
   @override
   List<Object?> get props => [message];
 }
+
 class OtpError extends OtpState {
   final String message;
   OtpError(this.message);
   @override
   List<Object?> get props => [message];
+}
+
+class OtpBloc extends Bloc<OtpEvent, OtpState> {
+  
 }
