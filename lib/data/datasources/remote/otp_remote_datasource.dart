e7f3f8db42209273ad1afbac97f2d27d3a883678
CoupDerace <turtusibae@gmail.com>
2026-06-13 14:07:59 +0700

    feat(otp): implement OTP remote datasource
    cakupan: implementasi OTP remote datasource menggunakan ApiClient

--- lib/data/datasources/remote/otp_remote_datasource.dart
+++ lib/data/datasources/remote/otp_remote_datasource.dart
@@ -1,9 +1,12 @@
-
-
 abstract class OtpRemoteDatasource {
   Future<OtpSentEntity> sendOtpFirebase();
   Future<OtpSentEntity> sendOtpEmail();
   Future<void> confirmOtp({required String code, required String otpType});
   Future<TotpSetupEntity> registerTotp();
   Future<bool> verifyTotp(String code);
+}
+
+class OtpRemoteDatasourceImpl implements OtpRemoteDatasource {
+  final ApiClient _client;
+  OtpRemoteDatasourceImpl(this._client);
 }
