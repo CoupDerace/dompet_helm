54c807fbe6af8f96d532cc3858940d31e1afebcd
CoupDerace <turtusibae@gmail.com>
2026-06-13 18:29:19 +0700

    feat(account): add immutable account entity constructor
    cakupan: constructor const untuk AccountEntity

--- lib/domain/entitas/account_entity.dart
+++ lib/domain/entitas/account_entity.dart
@@ -3,4 +3,11 @@ class AccountEntity extends Equatable {
   final int userId;
   final double balance;
   final DateTime createdAt;
+
+  const AccountEntity({
+    required this.id,
+    required this.userId,
+    required this.balance,
+    required this.createdAt,
+  });
 }
