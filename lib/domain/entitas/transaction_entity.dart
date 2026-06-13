4b4aa51949da16010d06cc7de0a6fb7590d7eb7d
CoupDerace <turtusibae@gmail.com>
2026-06-13 18:55:25 +0700

    feat(transaction): add transaction entity properties
    cakupan: penambahan informasi transaksi dan saldo pada TransactionEntity

--- lib/domain/entitas/transaction_entity.dart
+++ lib/domain/entitas/transaction_entity.dart
@@ -1,5 +1,14 @@
 enum TransactionType { credit, debit }
 
 class TransactionEntity extends Equatable {
+  final int id;
+  final int accountId;
+  final double amount;
+  final TransactionType type;
+  final String description;
+  final double balanceBefore;
+  final double balanceAfter;
+  final DateTime createdAt;
+
   
 }
