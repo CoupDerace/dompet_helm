95daa4321bd322dd4dec2a1ee5fcb69da9d642f5
CoupDerace <turtusibae@gmail.com>
2026-06-13 11:55:11 +0700

    feat(auth): implement auth remote datasource
    cakupan: implementasi autentikasi remote menggunakan ApiClient

--- lib/data/datasources/remote/auth_remote_datasource.dart
+++ lib/data/datasources/remote/auth_remote_datasource.dart
@@ -1,10 +1,19 @@
-
-
 abstract class AuthRemoteDatasource {
-  Future<({UserModel user, String token})> verifyFirebaseToken(String firebaseToken);
-  Future<({UserModel user, String token})> registerWithOtp(String firebaseToken);
+  Future<({UserModel user, String token})> verifyFirebaseToken(
+    String firebaseToken,
+  );
+  Future<({UserModel user, String token})> registerWithOtp(
+    String firebaseToken,
+  );
   Future<void> verifyEmailOtp(String code);
   Future<UserModel> getMe();
   Future<void> updateFcmToken(String fcmToken);
   void clearAuthToken();
+}
+
+class AuthRemoteDatasourceImpl implements AuthRemoteDatasource {
+  final ApiClient _client;
+  AuthRemoteDatasourceImpl(this._client);
+
+
 }
