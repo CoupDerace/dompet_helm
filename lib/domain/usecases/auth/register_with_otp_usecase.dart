7c574bab80c3c09b99950329813acf78b4b038ed
CoupDerace <turtusibae@gmail.com>
2026-06-13 20:33:56 +0700

    feat(auth): add registration execution with firebase token
    cakupan: eksekusi registerWithOtp menggunakan firebase token

--- lib/domain/usecases/auth/register_with_otp_usecase.dart
+++ lib/domain/usecases/auth/register_with_otp_usecase.dart
@@ -1,4 +1,8 @@
 class RegisterWithOtpUsecase {
   final AuthRepository _repository;
   RegisterWithOtpUsecase(this._repository);
+
+  Future<({UserEntity user, String token})> call(String firebaseToken) {
+    return _repository.registerWithOtp(firebaseToken);
+  }
 }
