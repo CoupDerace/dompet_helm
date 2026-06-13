81fdd2bdd7fd79edf1a92e932c475d73004604a5
CoupDerace <turtusibae@gmail.com>
2026-06-13 09:54:36 +0700

    feat(theme): enable Material 3 design system
    cakupan: aktivasi Material 3 pada aplikasi

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,6 +2,9 @@ class AppTheme{
    static ThemeData get light {
     const fontFamily = 'PlusJakartaSans';
     return ThemeData(
+      useMaterial3: true,
+      fontFamily: fontFamily,
+      
     )
   }
 }
