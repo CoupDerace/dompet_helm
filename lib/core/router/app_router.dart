3303d4c2a57756b0050a89729add42d607e24957
CoupDerace <turtusibae@gmail.com>
2026-06-13 08:50:56 +0700

    feat(router): add app navigation and route management using GoRouter
    cakupan semua router

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,50 @@
+class AppRouter {
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+  static GoRouter get router => GoRouter(
+        navigatorKey: _rootNavigatorKey,
+        initialLocation: '/splash',
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (_, __) => _withAuth(const SplashPage()),
+          ),
+          GoRoute(
+            path: '/login',
+            builder: (_, __) => _withAuth(const LoginPage()),
+          ),
+          GoRoute(
+            path: '/register',
+            builder: (_, __) => const RegisterPage(),
+          ),
+          GoRoute(
+            path: '/verify-email',
+            builder: (_, __) => const VerifyEmailPage(),
+          ),
+          GoRoute(
+            path: '/setup-2fa',
+            builder: (_, __) => const Setup2FAPage(),
+          ),
+           GoRoute(
+            path: '/2fa/smtp',
+            builder: (_, state) {
+              final extra = state.extra as Map<String, dynamic>?;
+              return _withOtp(TwoFASmtpPage(mode: extra?['mode'] as String? ?? 'login'));
+            },
+          ),
+          GoRoute(
+            path: '/2fa/totp',
+            builder: (_, state) {
+              final extra = state.extra as Map<String, dynamic>?;
+              return _withOtp(TwoFATotpPage(mode: extra?['mode'] as String? ?? 'login'));
+            },
+          ),
+          GoRoute(
+            path: '/2fa/notif',
+            builder: (_, state) {
+              final extra = state.extra as Map<String, dynamic>?;
+              return _withOtp(TwoFANotifPage(mode: extra?['mode'] as String? ?? 'login'));
+            },
+          ),
+
+}
