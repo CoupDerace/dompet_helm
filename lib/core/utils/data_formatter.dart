e8bfaabec56321295eff7d744c39c837dd2474a8
CoupDerace <turtusibae@gmail.com>
2026-06-13 10:06:21 +0700

    feat(utils): add relative date formatter helper
    cakupan: format tanggal dengan label Hari ini dan Kemarin

---
+++ lib/core/utils/data_formatter.dart
@@ -0,0 +1,10 @@
+class DateFormatter {
+  static String format(DateTime dt) {
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    final yesterday = today.subtract(const Duration(days: 1));
+    final date = DateTime(dt.year, dt.month, dt.day);
+  
+  
+  }
+}
