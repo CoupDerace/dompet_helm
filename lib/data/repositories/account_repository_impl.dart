e4ecfdafdc982d262991dc018a30b0003b9ebc58
CoupDerace <turtusibae@gmail.com>
2026-06-13 17:28:21 +0700

    feat(account): add get account repository handler
    cakupan: pengambilan data account melalui repository

--- lib/data/repositories/account_repository_impl.dart
+++ lib/data/repositories/account_repository_impl.dart
@@ -2,5 +2,14 @@ class AccountRepositoryImpl implements AccountRepository {
   final AccountRemoteDatasource _remote;
   AccountRepositoryImpl(this._remote);
 
-  
+  @override
+  Future<AccountEntity> getAccount() async {
+    try {
+      return await _remote.getAccount();
+    } on ServerException catch (e) {
+      throw ServerFailure(e.message);
+    } on NetworkException catch (e) {
+      throw NetworkFailure(e.message);
+    }
+  }
 }
