d0c8f193d436f267037b2c8b87d88e7b8f59f105
CoupDerace <turtusibae@gmail.com>
2026-06-13 10:03:39 +0700

    feat(utils): add currency formatting helper
    cakupan: helper format nominal double menjadi format rupiah

--- lib/core/utils/currency_formatter.dart
+++ lib/core/utils/currency_formatter.dart
@@ -1,4 +1,10 @@
 class CurrencyFormatter {
   static final _formatter = NumberFormat('#,###', 'id_ID');
 
+  static String format(double amount, {bool withSymbol = true}) {
+    final formatted = _formatter.format(amount.abs().round());
+    return withSymbol ? 'Rp$formatted' : formatted;
+  }
+
+  
 }
