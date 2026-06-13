602f96042c8c08cdbbdb02851deba57a74a4893d
CoupDerace <turtusibae@gmail.com>
2026-06-13 09:59:55 +0700

    feat(bloc): add bloc creation logging
    cakupan: logging saat bloc dibuat melalui onCreate

--- lib/core/utils/app_bloc_observer.dart
+++ lib/core/utils/app_bloc_observer.dart
@@ -1,4 +1,11 @@
 class AppBlocObserver extends BlocObserver {
   const AppBlocObserver();
+
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    debugPrint('[BLoC] CREATE  ${bloc.runtimeType}');
+  }
+
   
 }
