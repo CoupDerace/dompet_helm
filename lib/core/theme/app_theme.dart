145415f4f7ecac14081ef7cb96227da7ec272ff2
CoupDerace <turtusibae@gmail.com>
2026-06-13 09:55:23 +0700

    feat(theme): add custom color scheme configuration
    cakupan: konfigurasi ColorScheme menggunakan AppColors

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,9 +2,17 @@ class AppTheme{
    static ThemeData get light {
     const fontFamily = 'PlusJakartaSans';
     return ThemeData(
-      useMaterial3: true,
-      fontFamily: fontFamily,
-      
+        useMaterial3: true,
+        fontFamily: fontFamily,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.primary,
+        primary: AppColors.primary,
+        onPrimary: Colors.white,
+        secondary: AppColors.green,
+        surface: AppColors.white,
+        background: AppColors.bg,
+        error: AppColors.red,
+      ),
     )
   }
 }
