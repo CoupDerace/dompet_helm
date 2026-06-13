6e8ca076663f07f0ecadd3b3447ecd9ec772bafd
CoupDerace <turtusibae@gmail.com>
2026-06-13 14:12:04 +0700

    feat(payment): implement payment remote datasource
    cakupan: implementasi payment datasource menggunakan ApiClient

--- lib/data/datasources/remote/payment_remote_datasource.dart
+++ lib/data/datasources/remote/payment_remote_datasource.dart
@@ -1,5 +1,3 @@
-
-
 abstract class PaymentRemoteDatasource {
   Future<({double balance, double amount})> topup(double amount);
   Future<TransferResultEntity> transfer({
@@ -8,4 +6,10 @@ abstract class PaymentRemoteDatasource {
     required String otpCode,
     required String otpType,
   });
+}
+
+class PaymentRemoteDatasourceImpl implements PaymentRemoteDatasource {
+  final ApiClient _client;
+  PaymentRemoteDatasourceImpl(this._client);
+
 }
