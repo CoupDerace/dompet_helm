ff8631920bc84527bf60d99fde6daf0214f689e4
CoupDerace <turtusibae@gmail.com>
2026-06-13 18:43:21 +0700

    feat(payment): add payment result entity properties
    cakupan: penambahan title, subtitle, amount, lines, dan kind pada PaymentResultEntity

--- lib/domain/entitas/payment_result_entity.dart
+++ lib/domain/entitas/payment_result_entity.dart
@@ -1,3 +1,7 @@
 class PaymentResultEntity extends Equatable {
-  
+  final String title;
+  final String subtitle;
+  final double amount;
+  final List<List<String>> lines;
+  final String kind;
 }
