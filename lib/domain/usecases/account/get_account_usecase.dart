de08dd1117f6823032b1bc67d6b608bec4141f66
CoupDerace <turtusibae@gmail.com>
2026-06-13 20:24:33 +0700

    feat(transaction): add transaction repository injection in use case
    cakupan: inject AccountRepository ke GetTransactionsUsecase

--- lib/domain/usecases/account/get_account_usecase.dart
+++ lib/domain/usecases/account/get_account_usecase.dart
@@ -1,11 +1,11 @@
-
-
-class GetAccountUsecase{
+class GetAccountUsecase {
   final AccountRepository _repository;
   GetAccountUsecase(this._repository);
   Future<AccountEntity> call() => _repository.getAccount();
 }
 
-class GetTransactionsUsecase{
+class GetTransactionsUsecase {
+  final AccountRepository _repository;
+  GetTransactionsUsecase(this._repository);
   
 }
