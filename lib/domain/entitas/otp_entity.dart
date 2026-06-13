b345ec9e59456af93f5d0483ad59085245754408
CoupDerace <turtusibae@gmail.com>
2026-06-13 18:33:40 +0700

    feat(totp): add immutable TOTP setup constructor
    cakupan: constructor const untuk TotpSetupEntity

--- lib/domain/entitas/otp_entity.dart
+++ lib/domain/entitas/otp_entity.dart
@@ -4,5 +4,10 @@ class TotpSetupEntity extends Equatable {
   final String issuer;
   final String account;
 
-  
+  const TotpSetupEntity({
+    required this.secret,
+    required this.qrCode,
+    required this.issuer,
+    required this.account,
+  })
 }
