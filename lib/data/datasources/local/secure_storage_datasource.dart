9afc04e965f9ea6a380516527d263e561c6d81bc
CoupDerace <turtusibae@gmail.com>
2026-06-13 10:54:37 +0700

    feat(storage): implement secure storage datasource
    cakupan: implementasi FlutterSecureStorage pada SecureStorageDatasource

--- lib/data/datasources/local/secure_storage_datasource.dart
+++ lib/data/datasources/local/secure_storage_datasource.dart
@@ -12,3 +12,17 @@ abstract class SecureStorageDatasource {
   Future<bool> getAuthVerified();
   Future<void> clearAll();
 }
+
+class SecureStorageDatasourceImpl implements SecureStorageDatasource {
+  final FlutterSecureStorage _storage;
+
+  SecureStorageDatasourceImpl(this._storage);
+
+  @override
+  Future<void> saveToken(String token) async {
+    try {
+      await _storage.write(key: AppConstants.kJwtToken, value: token);
+    } catch (_) {
+      throw const CacheException('Gagal menyimpan token.');
+    }
+  }
