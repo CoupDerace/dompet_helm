72a40b4ea881e0440934e5ebefca021bd8924dc9
CoupDerace <turtusibae@gmail.com>
2026-06-13 14:17:13 +0700

    feat(account): add account model implementation
    cakupan: implementasi AccountModel yang extends AccountEntity

---
+++ lib/data/models/account_model.dart
@@ -0,0 +1,11 @@
+
+
+class AccountModel extends AccountEntity {
+  const AccountModel({
+    required super.id,
+    required super.userId,
+    required super.balance,
+    required super.createdAt,
+  });
+
+  
