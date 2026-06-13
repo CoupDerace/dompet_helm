7c6b476f113fad0a84e5f8ab82207a77324ccc64
CoupDerace <turtusibae@gmail.com>
2026-06-13 22:33:53 +0700

    feat(account): implement account bloc
    cakupan: implementasi AccountBloc menggunakan flutter_bloc

--- lib/presentation/blocs/account/account_bloc.dart
+++ lib/presentation/blocs/account/account_bloc.dart
@@ -1,19 +1,22 @@
-
-
 // Events
 abstract class AccountEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
 class AccountLoadRequested extends AccountEvent {}
+
 class AccountRefreshRequested extends AccountEvent {}
 
 abstract class AccountState extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
 class AccountInitial extends AccountState {}
+
 class AccountLoading extends AccountState {}
+
 class AccountLoaded extends AccountState {
   final AccountEntity account;
   final List<TransactionEntity> transactions;
@@ -21,9 +24,14 @@ class AccountLoaded extends AccountState {
   @override
   List<Object?> get props => [account, transactions];
 }
+
 class AccountError extends AccountState {
   final String message;
   AccountError(this.message);
   @override
   List<Object?> get props => [message];
+}
+
+class AccountBloc extends Bloc<AccountEvent, AccountState> {
+  
 }
