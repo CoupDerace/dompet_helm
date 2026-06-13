7300401440c39a57eff1d5417757c5fe446cdaa5
CoupDerace <turtusibae@gmail.com>
2026-06-13 17:31:52 +0700

    feat(auth): add auth repository dependency injection
    cakupan: inject AuthRemoteDatasource dan SecureStorageDatasource ke repository

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -1,3 +1,8 @@
 class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDatasource _remote;
+  final SecureStorageDatasource _local;
+
+  AuthRepositoryImpl(this._remote, this._local);
+
   
 }
