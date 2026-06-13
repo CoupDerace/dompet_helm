63dd017efb8ef566ed2e33c964058c7701d86f61
CoupDerace <turtusibae@gmail.com>
2026-06-13 11:55:30 +0700

    feat(auth): add firebase token verification handler
    cakupan: login menggunakan firebase token dan parsing access token beserta user data

--- lib/data/datasources/remote/auth_remote_datasource.dart
+++ lib/data/datasources/remote/auth_remote_datasource.dart
@@ -15,5 +15,16 @@ class AuthRemoteDatasourceImpl implements AuthRemoteDatasource {
   final ApiClient _client;
   AuthRemoteDatasourceImpl(this._client);
 
-
+  @override
+  Future<({UserModel user, String token})> verifyFirebaseToken(String firebaseToken) async {
+    final response = await _client.post(
+      ApiEndpoints.verifyToken,
+      data: {'firebase_token': firebaseToken},
+    );
+    final data = response['data'] as Map<String, dynamic>;
+    final token = data['access_token'] as String;
+    final user = UserModel.fromJson(data['user'] as Map<String, dynamic>);
+    _client.setAuthToken(token);
+    return (user: user, token: token);
+  }
 }
