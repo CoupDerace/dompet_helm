0023b6d9f31feea97f43ee0c146e111be3bc6f07
CoupDerace <turtusibae@gmail.com>
2026-06-13 09:20:17 +0700

    feat(theme): add primary gradient configuration
    cakupan: gradient utama aplikasi menggunakan kombinasi warna primary

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -29,5 +29,11 @@ class AppColors{
   static const Color bg = Color(0xFFF6F7F9);
   static const Color white = Color(0xFFFFFFFF);
 
-  
+  // Gradient
+  static const LinearGradient primaryGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [0.0, 0.55, 1.0],
+    colors: [primaryLight, primary, primaryDark],
+  );
 }
