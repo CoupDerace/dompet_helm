594b91fa54af713beb2e444b78f95c644c478639
CoupDerace <turtusibae@gmail.com>
2026-06-13 17:44:14 +0700

    feat(otp): add Firebase OTP repository handler
    cakupan: pengiriman OTP Firebase melalui repository

--- lib/data/repositories/otp_repository_impl.dart
+++ lib/data/repositories/otp_repository_impl.dart
@@ -3,5 +3,14 @@ class OtpRepositoryImpl implements OtpRepository {
   final AuthRepository _authRepo;
   OtpRepositoryImpl(this._remote, this._authRepo);
 
-  
+  @override
+  Future<OtpSentEntity> sendOtpFirebase() async {
+    try {
+      return await _remote.sendOtpFirebase();
+    } on ServerException catch (e) {
+      throw ServerFailure(e.message);
+    } on NetworkException catch (e) {
+      throw NetworkFailure(e.message);
+    }
+  }
 }
