56577367bdd0bd992d9ad9c774c2ad53a42d3697
CoupDerace <turtusibae@gmail.com>
2026-06-13 09:13:47 +0700

    refactor(auth): add auth and OTP bloc wrapper helpers
    cakupan: helper provider untuk AuthBloc dan OtpBloc

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -131,4 +131,16 @@ class AppRouter {
           ),
           GoRoute(path: '/merchant', builder: (_, __) => _withPayment(const MerchantCheckoutPage())),
         ],
+      );
+      
+  static Widget _withAuth(Widget child) {
+    return BlocProvider(create: (_) => sl<AuthBloc>(), child: child);
+  }
+
+  static Widget _withOtp(Widget child) {
+    return MultiBlocProvider(providers: [
+      BlocProvider(create: (_) => sl<AuthBloc>()),
+      BlocProvider(create: (_) => sl<OtpBloc>()),
+    ], child: child);
+  }
 }
