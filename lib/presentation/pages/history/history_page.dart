ce0f92115a0652070a64aa963256e27050d69a15
CoupDerace <turtusibae@gmail.com>
2026-06-14 17:55:58 +0700

    feat(ui): membuat layout utama halaman riwayat
    menambahkan scaffold dan struktur dasar tampilan halaman history transaksi

--- lib/presentation/pages/history/history_page.dart
+++ lib/presentation/pages/history/history_page.dart
@@ -14,3 +14,17 @@ class _HistoryPageState extends State<HistoryPage> {
     super.initState();
     context.read<AccountBloc>().add(AccountLoadRequested());
   }
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.bg,
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.fromLTRB(20, MediaQuery.of(context).padding.top + 12, 20, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+  }
