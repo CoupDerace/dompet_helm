f379904e3c3c71d33c93816bf0389c4b39b66682
CoupDerace <turtusibae@gmail.com>
2026-06-14 16:38:39 +0700

    feat(auth): membuat halaman autentikasi 2FA TOTP
    menambahkan widget TwoFATotpPage untuk autentikasi dua langkah menggunakan aplikasi authenticator dengan mode login dan setup

---
+++ lib/presentation/pages/auth/twofa_totp_pages.dart
@@ -0,0 +1,8 @@
+
+
+class TwoFATotpPage extends StatefulWidget {
+  final String mode;
+  const TwoFATotpPage({super.key, this.mode = 'login'});
+  @override
+  State<TwoFATotpPage> createState() => _TwoFATotpPageState();
+}
